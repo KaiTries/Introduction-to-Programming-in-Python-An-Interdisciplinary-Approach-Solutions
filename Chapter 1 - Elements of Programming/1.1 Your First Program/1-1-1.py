cb51bb8b7bdf1a62cb03a625ddf5b7f21fe4c646
KaiTries <112092835+KaiTries@users.noreply.github.com>
2023-02-20 18:56:12 +0100

    first exercise uploaded

--- Chapter 1 - Elements of Programming/1.1 Your First Program/1-1-1.py
+++ Chapter 1 - Elements of Programming/1.1 Your First Program/1-1-1.py
@@ -1,1 +1,13 @@
 #Compose a program that writes the Hello, World message 10 times.
+import stdio
+
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
+stdio.writeln('Hello, World')
