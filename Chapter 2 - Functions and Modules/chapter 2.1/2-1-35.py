0fa6134d6129a698cff278e6319a613e356807d7
KaiTries <112092835+KaiTries@users.noreply.github.com>
2023-03-11 14:35:31 +0100

    updated answer for calender.py

--- Chapter 2 - Functions and Modules/chapter 2.1/2-1-35.py
+++ Chapter 2 - Functions and Modules/chapter 2.1/2-1-35.py
@@ -1,83 +1,101 @@
-#calender 
 import stdio
 import sys
 
-#------ Global Variables
-months = ["January","February","March","April","Mai","June","July","August","September","October","November","Dezember"]
-days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
 
-#------ Functions
 
+#------global variables
 
-#------ Leapyear
-def _isleap(y):
+mon = ["January","February","March","April","Mai","June","July","August","September","October","November","Dezember"]
+day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+#------functions
+
+#------check if leap year
+
+def _leap(y):
     isLeapYear = (y % 4 == 0)
     isLeapYear = isLeapYear and (y % 100 != 0)
     isLeapYear = isLeapYear or  (y % 400 == 0)
     return isLeapYear
 
+#------day assigmnet -> falls on which weekday
 
-#------ day 0-6
-def _day(m,d,y):
+def _day(y,m,d):
     y0 = y - (14 - m) // 12
     x = y0 + y0//4 - y0//100 + y0//400
     m0 = m + 12 * ((14 - m) // 12) - 2
     d0 = (d + x + (31*m0)//12) % 7
     return d0
 
+#------define length of month and fill with weekdays
 
-
-#------ Length of Month
-def _month(m,y):
-    a = []
+def _month(y,m):
     if m == 2:
-        if _isleap(y):
+        if _leap(y):
             days = 29
-            for i in range(days):
-                x = _day(m,i,y)
-                a += [x]
+            a = []
+            for i in range(1,days+1):
+                a += [_day(y,m,i)]
             return a
         else:
             days = 28
-            for i in range(days):
-                x = _day(m,i,y)
-                a += [x]
-            return a            
-    elif m%2 == 0 and m != 8:
+            a = []
+            for i in range(1,days+1):
+                a += [_day(y,m,i)]           
+            return a
+    elif m%2 != 0 or m == 8:
+        days = 31
+        a = []
+        for i in range(1,days+1):
+            a += [_day(y,m,i)]           
+        return a       
+    else:
         days = 30
-        for i in range(days):
-            x = _day(m,i,y)
-            a += [x]
+        a = []
+        for i in range(1,days+1):
+            a += [_day(y,m,i)]           
+        return a          
+
+#------add filler so that calender is aligned
+
+def _fixedSun(a):
+    if a[0] == 0:
         return a
     else:
-        days = 31
-        for i in range(days):
-            x = _day(m,i,y)
-            a += [x]
-        return a        
+        diff = a[0]
+        x = []
+        for i in range(diff):
+            x += [0]
+        x += a
+        return x
 
+#------Full function
 
-#------ Input
-m = int(sys.argv[1])
-y = int(sys.argv[2])
+def calender(y,m):
+    a = _month(y,m)
+    x = _fixedSun(a)
+    diff = (len(x)-len(a))-1
+    empty = " "
+    stdio.writef('%12.10s %4.4s \n', mon[m-1],y)
+    for i in range(len(day)):
+        stdio.writef('%3.2s',day[i])
+    stdio.writeln()  
+    for i in range(len(x)):
+        if i <= diff:
+            stdio.writef('%3.2s',empty)
+        elif (i+1)%7 == 0:
+            stdio.writef('%3.2s \n',i-diff)        
+        else:
+            stdio.writef('%3.2s',i-diff)
+    return stdio.writeln()
 
-#------ Processing
+#------global code 
 
-x = _month(m,y)
-
-
-
-#------ Output
-stdio.writef('%12.10s %4s \n',months[m-1],y)
-for i in range(7):
-    stdio.writef('%-4.3s',days[x[i]])
+stdio.write("Please enter year: ")
+y = stdio.readInt()
+stdio.write("Please enter month: ")
+m = stdio.readInt()
 stdio.writeln()
-for i in range(len(x)):
-    if (i+1)%7 == 0 and i != 0:
-        stdio.writef('%3.3s \n',i+1)
-    else:
-        stdio.writef('%3.3s ',i+1)
 
-
-#couldnt figure out how to adapt the numbers to the days so i adapted starting day to number 1
+calender(y,m)
